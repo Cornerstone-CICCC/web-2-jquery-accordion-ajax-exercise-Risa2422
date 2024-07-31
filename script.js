d10d0bf77a197f269158d398babddca72a5f7fb3
Risa Yamamoto <yamari2422@gmail.com>
2024-07-31 16:01:21 -0700

    finished exercise2 for now

--- script.js
+++ script.js
@@ -7,4 +7,33 @@ $(function () {
     $(this).next().slideToggle();
     accordion.find("h3").not($(this)).next().slideUp();
   });
+
+  // exercise2
+  const button = $(".todos");
+  button.find("button").on("click", async function () {
+    const data = await getData();
+    console.log(data);
+
+    const todoList = $("ul");
+    let text = "";
+    for (let i = 0; i < data.todos.length; i++) {
+      text += `<li>${data.todos[i].todo}</li>`;
+    }
+    todoList.html(text);
+  });
+
+  function getData() {
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: `https://dummyjson.com/todos`,
+        type: `GET`,
+        success: function (response) {
+          resolve(response);
+        },
+        error: function (error) {
+          reject(error);
+        },
+      });
+    });
+  }
 });
