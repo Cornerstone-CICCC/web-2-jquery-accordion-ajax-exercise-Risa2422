2d8fe7495c61656db5e78ea440348dad461d1ba3
Risa Yamamoto <yamari2422@gmail.com>
2024-07-31 15:50:04 -0700

    finished exercise1

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-$(function() {
+$(function () {
   // your code here
-})
+
+  // exercise1
+  const accordion = $(".accordion");
+  accordion.find("h3").on("click", function () {
+    $(this).next().slideToggle();
+    accordion.find("h3").not($(this)).next().slideUp();
+  });
+});
